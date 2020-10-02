f31c45c467118682a4cda1987caaf45a3542a486
Tyler-Weber <817230@mystma.org>
2020-10-02 20:02:40 +0000

    5 Question Quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,24 +1,55 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-for (int i = 10; i <= 20; i = i +1){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting.");
-}
-for (int i = 10; i <= 100; i = i +1){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting.");
-}
-for (int i = 1000; i <= 256000; i = i +1){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting.");
-}
-for (int i = 11; i <= 12; i = i +1){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting.");
-}
-for (int i = 10; i >= 0; i = i -1){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting.");
-}}}
+        Scanner input = new Scanner(System.in);
+        System.out.println("Did the Twins win the series?");
+        String correctAnswer1 = "no";
+        String answer1 = input.nextLine();
+
+        if (answer1.equals(correctAnswer1))
+            System.out.println("Correct!");
+        else {
+            System.out.println("Wrong");
+        }
+
+        System.out.println("Who is the Vikings Quarterback?");
+        String correctAnswer2 = "Kirk Cousins";
+        String answer2 = input.nextLine();
+
+        if (answer2.equals(correctAnswer2))
+            System.out.println("Correct");
+        else {
+            System.out.println("Wrong");
+        }
+    System.out.println("What is our school mascot?");
+        String correctAnswer3 = "Knight";
+        String answer3 = input.nextLine();
+
+        if (answer3.equals(correctAnswer3))
+            System.out.println("Correct!");
+        else {
+            System.out.println("Wrong");
+        }
+System.out.println("When were STMA schools founded?");
+        String correctAnswer4 = "1969";
+        String answer4 = input.nextLine();
+
+        if (answer4.equals(correctAnswer4))
+            System.out.println("Correct!");
+        else {
+            System.out.println("Wrong");
+        }
+System.out.println("What is the Intro to Programming teacher's last name?");
+        String correctAnswer5 = "Osowski";
+        String answer5 = input.nextLine();
+
+        if (answer5.equals(correctAnswer5))
+            System.out.println("Correct!");
+        else {
+            System.out.println("Wrong");
+        }
+
+}}
