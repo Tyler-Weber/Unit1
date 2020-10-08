dea1fe0d123c7e417b3f3dce59296e049f46d07b
Tyler-Weber <817230@mystma.org>
2020-10-08 19:32:58 +0000

    0-1000 in 2 Lines of Code

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,40 +1,15 @@
 package app;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Penguin Fun Facts! Type WHOLE #s 1-5 to Learn!");
-        String factOne = "1";
-        String factTwo = "2";
-        String factThree = "3";
-        String factFour = "4";
-        String factFive = "5";
-        String number = input.nextLine();
-        if (number.equals(factOne))
-            System.out.println("Penguins are flightless birds.");
+        for (int i = 0; i <= 1000; i = i +1){
+        System.out.println(i);
+        }
 
-        else {System.out.println("Error");}
 
-        if (number.equals(factTwo))
-            System.out.println("Penguins can stay underwater for up to 20 minutes at a time.");
 
-        else {System.out.println("Error");}
 
-        if (number.equals(factThree))
-            System.out.println("Penguins don't have teeth.");
 
-        else {System.out.println("Error");}
 
-        if (number.equals(factFour))
-            System.out.println("Penguins can drink sea water.");
-        
-        else {System.out.println("Error");}
-
-        if (number.equals(factFive))
-            System.out.println("Most Penguins live in the southern hemisphere.");
-        
-        else {System.out.println("Error");}
     }
 }
