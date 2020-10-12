80fb9fbd6bc83ff3ab29e5132b52c9f144b6595c
Tyler-Weber <817230@mystma.org>
2020-10-12 20:14:24 +0000

    Password Challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,15 +1,37 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i <= 1000; i = i +1){
-        System.out.println(i);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter Secret Password");
+        String code = "We have the best football team in the state";
+        String password = input.nextLine();
+
+        if (password.equals("stmaknights")) {
+            System.out.println(code);
+        } else {
+            System.out.println("Incorrect, 2 Attempts Left");
+
+            input.nextLine();
+            if (password.equals("stmaknights")) {
+                System.out.println(code);
+            } else {
+                System.out.println("Incorrect, 1 Attempts Left");
+
+                input.nextLine();
+                if (password.equals("stmaknights")) {
+                    System.out.println(code);
+                } else {
+                    System.out.println("Incorrect, 0 Attempts Left");
+
+                }
+
+            }
+
         }
 
-
-
-
-
-
     }
+
 }
