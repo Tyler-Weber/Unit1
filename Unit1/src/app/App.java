f706b179d3ca0224e983ea9e2071899425196376
Tyler-Weber <817230@mystma.org>
2020-10-13 19:42:57 +0000

    Even Number Generator Challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,24 +5,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        int targetNumber = 7;
-        boolean isFinished = false;
+    System.out.println("Even Number Generator");
+    System.out.println("Please type in a number");
+    int number1 = input.nextInt();
+    System.out.println("Please enter a Higher number");
+    int number2 = input.nextInt();
+    for(int i = number1; i < number2; i++) {
 
-        while (isFinished == false) {
-            System.out.println("Guess a number between 0 and 100");
-            int guess = input.nextInt();
-            input.nextLine();
-            if (guess == targetNumber) {
-                System.out.println("Correct!");
-                isFinished = true;
-            } else {
-                if (guess > targetNumber) {
-                    System.out.println("Lower");
-                } else {
-                    System.out.println("Higher");
-                }
-            }
+        if( i % 2 == 0) {
+            System.out.println(i);
         }
 
+
     }
-}
+
+
+
+
+    }}
